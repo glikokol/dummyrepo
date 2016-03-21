c12dce10c6e018180d2a808ab827c762bfab2921
glikokol <blagojivkov@gmail.com>
2016-03-21 22:15:37 +0200

    feature branch first change

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(int argc, char argv[]) {
+	std::cout << "Feaure branch, First change!" << std::endl;
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "Master branch, first change!" std::endl;
 	std::cout << "Master branch, 2nd change!" std::endl;

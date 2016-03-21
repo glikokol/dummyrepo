9eb3b751134a4bf3d851651fca354ec1adf991a5
glikokol <blagojivkov@gmail.com>
2016-03-21 22:35:21 +0200

    feature, 4th change

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main(int argc, char argv[]) {
 	std::cout << "Master branch, first change!" std::endl;
 	std::cout << "Master branch, 2nd change!" std::endl;
 	std::cout << "Master branch, 3nd change!" std::endl;
+	std::cout << "Master branch, 4nd change!" std::endl;
 }
 

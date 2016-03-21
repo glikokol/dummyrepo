7cf78e9263e6f0b12617d40c77cf22d64bf3dc9b
glikokol <blagojivkov@gmail.com>
2016-03-21 22:25:39 +0200

    master, 3rd change

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main(int argc, char argv[]) {
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "Master branch, first change!" std::endl;
 	std::cout << "Master branch, 2nd change!" std::endl;
+	std::cout << "Master branch, 3nd change!" std::endl;
 }
 
